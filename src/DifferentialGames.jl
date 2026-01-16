e11d6b1ca3040ec8f3a251ea5511132d4ff5e092
BennetOutland <bennet.outland@pm.me>
2026-01-16 15:00:42 -0700

    re-structured to encapsulate the primary package interface in DifferentialGamesBase. This repo will just contain some reference solvers for easy starting/trialing

--- src/DifferentialGames.jl
+++ src/DifferentialGames.jl
@@ -1,37 +1,12 @@
 module DifferentialGames
 
 # Includes
-include("problems/base.jl")
-include("core/constraints.jl")
-include("core/player.jl")
-include("problems/GNEP.jl")
+# include("problems/base.jl")
+# include("core/constraints.jl")
+# include("core/player.jl")
+# include("problems/GNEP.jl")
 
 # Exports
-export 
-    # Abstract Types 
-    DifferentialGame, 
-    InverseDifferentialGame,
-    AbstractLQGame,
-
-    # Player-based Interface 
-    ConstraintSpec,
-    Player,
-
-    # Problems 
-    AbstractGNEP, 
-    AbstractPDGNEP, 
-    AbstractPotentialGame, 
-    AbstractLexicographicGame, 
-    AbstractLQGame, 
-    LQGameProblem, 
-    GNEProblem,
-    PDGNEProblem, 
-    PotentialGameProblem, 
-    LexicographicGameProblem, 
-
-    # Helper functions
-    num_players, 
-    state_dim, 
-    control_dim
+# export 
 
 end
--- src/utils/plotting.jl
+++
@@ -1,1 +0,0 @@
-println("WIP Plotting")
